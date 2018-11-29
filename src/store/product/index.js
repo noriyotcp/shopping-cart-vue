3c0de05d45ddac776c20d9d277a747634eda47ec
noriyotcp <noriyo.akita@gmail.com>
2018-11-30 03:03:29 +0900

    Install Vuex and setup

---
+++ src/store/product/index.js
@@ -0,0 +1,23 @@
+const state = {
+  productItems: []
+}
+
+const getters = {
+
+}
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const productModule = {
+  state,
+  getters,
+  mutations,
+  actions
+}
+
+export default productModule
